eca2f2af90e560c99a8b8ff992cbf1c33269bbc0
Hatsu Gen <phngphat226@gmail.com>
2026-04-14 13:08:21 +0700

    xoa di up lai
    Lỗi file

--- project/TravelGuide/Helpers/NetworkHelper.cs
+++
@@ -1,9 +0,0 @@
-// Helpers/NetworkHelper.cs
-namespace TravelGuide.Helpers
-{
-    public static class NetworkHelper
-    {
-        public static bool IsConnected =>
-            Connectivity.Current.NetworkAccess == NetworkAccess.Internet;
-    }
-}
